63ef7e4c9ca2e700ac26fe95f4a037d40acfebad
likhith-9 <likhithkrishna72@gmail.com>
2022-08-24 20:03:55 +0530

    Section 2

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/DataModels/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StudentAdminPortal.API.DataModels
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string PostalAddress { get; set; }
+
+        //Navigation properties
+        public Guid StudentId { get; set; }
+
+    }
+}
---
+++ StudentAdminPortal.API/StudentAdminPortal.API/DomainModels/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StudentAdminPortal.API.DomainModels
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string PostalAddress { get; set; }
+
+        //Navigation properties
+        public Guid StudentId { get; set; }
+    }
+}
