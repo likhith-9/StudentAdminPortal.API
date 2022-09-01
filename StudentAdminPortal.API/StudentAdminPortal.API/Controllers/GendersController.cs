e31dfecde3638b6d5a7cfbe589a8c792b64afec0
likhith-9 <likhithkrishna72@gmail.com>
2022-09-01 12:41:10 +0530

    sec 5

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/GendersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using StudentAdminPortal.API.Repositories;
+using System.Linq;
+using System.Collections.Generic;
+using StudentAdminPortal.API.DomainModels;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Controllers
+{
+    [ApiController]
+    public class GendersController : Controller
+    {
+        private readonly IStudentRepository studentRepository;
+        private readonly IMapper mapper;
+
+        public GendersController(IStudentRepository studentRepository, IMapper mapper)
+        {
+            this.studentRepository = studentRepository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        [Route("[controller]")]
+        public async Task<IActionResult> GetAllGenders()
+        {
+            var genderList = await studentRepository.GetGendersAsync();
+
+            if(genderList== null || !genderList.Any())
+            {
+                return NotFound();
+            }
+            return Ok(mapper.Map<List<Gender>>(genderList));
+        }
+    }
+}
